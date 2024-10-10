be73876f1773408d11afd5062b70ce21a026ad5b
carolinagallo <carolinagallo02@gmail.com>
2024-10-10 17:53:38 -0300

    ejercicio-3

---
+++ ejercicio-3/ejercicio3.h
@@ -0,0 +1,9 @@
+#ifndef EJERCICIO3_H_INCLUDED
+#define EJERCICIO3_H_INCLUDED
+#include "tad.h"
+
+void cargar_cola(Cola *cola);
+void recargar_colas(Cola *cola1, Cola *cola2, Cola cola_aux);
+void comparacion(Cola *cola1, Cola *cola2);
+
+#endif // EJERCICIO3_H_INCLUDED
