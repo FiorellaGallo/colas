28d7db7b374b2b54cc1d37b019dcda95446fef6e
María Agustina Ortiz <95661814+agustina-ortiz@users.noreply.github.com>
2024-10-10 17:31:46 -0300

    ejercicio 6

---
+++ Ejercicio6/ejercicio6.h
@@ -0,0 +1,10 @@
+#ifndef ejercicio6_h
+#define ejercicio6_h
+#include <stdio.h>
+#include "colas.h"
+#include "listas.h"
+#include "pilas.h"
+
+Lista encontrarComunes(Pila pila, Cola cola, Lista lista);
+
+#endif //ejercicio6_h
